d440b662e8c712701ad23e2a51b9b4b6e28ffdb0
Sungmin Woo <woos8899@gmail.com>
2024-12-04 16:28:20 +0900

    Replace `fmt::print` for `spdlog`

--- app/DisDatSim/main.cpp
+++ app/DisDatSim/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 
 #include <spdlog/spdlog.h>
 #include <asio.hpp>
@@ -268,6 +269,7 @@ namespace {
         const T* data, size_t size, size_t line_size, size_t indent_size
     ) {
         static_assert(sizeof(T) == 1, "sizeof(T) must be 1");
+        std::stringstream ss;
 
         for (size_t i = 0; i < size; ++i) {
             if (i % line_size == 0) {
@@ -278,9 +280,10 @@ namespace {
             }
 
             const auto byte = *reinterpret_cast<const uint8_t*>(data + i);
-            fmt::print("{:02X} ", byte);
+            ss << fmt::format("{:02X} ", byte);
         }
-        std::cout << '\n';
+
+        SPDLOG_INFO(ss.str());
     }
 
 
@@ -318,18 +321,22 @@ namespace {
 
             const auto line_count = bytes_transferred / 16;
 
-            fmt::print("\nBytes ({})\n", bytes_transferred);
+            std::stringstream ss;
+
+            ss << fmt::format("\nBytes ({})\n", bytes_transferred);
             //::print_bytes(recv_buffer_.data(), bytes_transferred, 16, 2);
 
             if (bytes_transferred < sizeof(::PduHeader)) {
-                fmt::print("Received bytes is less than PDU header size\n");
+                ss << fmt::format(
+                    "Received bytes is less than PDU header size\n"
+                );
                 return this->start_recv();
             }
 
             const auto pdu_header = reinterpret_cast<::PduHeader*>(
                 recv_buffer_.data()
             );
-            fmt::print(
+            ss << fmt::format(
                 "PDU Header: version={}, exercise_id={}, pdu_type={}, "
                 "protocol_family={}, timestamp={}, length={}\n",
                 pdu_header->version_,
@@ -341,13 +348,13 @@ namespace {
             );
 
             if (pdu_header->pdu_type_ == 1) {
-                fmt::print("Entity State PDU\n");
+                ss << fmt::format("Entity State PDU\n");
 
                 const auto entt_pdu = reinterpret_cast<::EnttPdu*>(
                     recv_buffer_.data()
                 );
 
-                fmt::print(
+                ss << fmt::format(
                     "  entt_id={}:{}:{}, force_id={}, "
                     "num_of_articulation_param={}\n",
                     entt_pdu->entt_id_.sim_addr_.site_id_.get(),
@@ -357,7 +364,7 @@ namespace {
                     entt_pdu->num_of_articulation_param_
                 );
 
-                fmt::print(
+                ss << fmt::format(
                     "  kind={}, domain={}, country={}, category={}, "
                     "subcategory={}, specific={}, extra={}\n",
                     entt_pdu->entt_type_.kind_,
@@ -369,7 +376,7 @@ namespace {
                     entt_pdu->entt_type_.extra_
                 );
 
-                fmt::print(
+                ss << fmt::format(
                     "  alt_kind={}, alt_domain={}, alt_country={}, "
                     "alt_category={}, alt_subcategory={}, alt_specific={}, "
                     "alt_extra={}\n",
@@ -382,21 +389,21 @@ namespace {
                     entt_pdu->alt_entt_type_.extra_
                 );
 
-                fmt::print(
+                ss << fmt::format(
                     "  linear_vel={{x={:.2f}, y={:.2f}, z={:.2f}}}\n",
                     entt_pdu->entt_linear_vel_.x_.get(),
                     entt_pdu->entt_linear_vel_.y_.get(),
                     entt_pdu->entt_linear_vel_.z_.get()
                 );
 
-                fmt::print(
+                ss << fmt::format(
                     "  loc={{x={:.2f}, y={:.2f}, z={:.2f}}}\n",
                     entt_pdu->entt_loc_.x_.get(),
                     entt_pdu->entt_loc_.y_.get(),
                     entt_pdu->entt_loc_.z_.get()
                 );
 
-                fmt::print(
+                ss << fmt::format(
                     "  orient={{psi={:.2f}, theta={:.2f}, "
                     "phi={:.2f}}}\n",
                     entt_pdu->entt_orient_.psi_.get(),
@@ -404,27 +411,27 @@ namespace {
                     entt_pdu->entt_orient_.phi_.get()
                 );
             } else if (pdu_header->pdu_type_ == 20) {
-                fmt::print("Data PDU\n");
+                ss << fmt::format("Data PDU\n");
 
                 const auto data_pdu = reinterpret_cast<::DataPdu*>(
                     recv_buffer_.data()
                 );
 
-                fmt::print(
+                ss << fmt::format(
                     "  originating_entt={}:{}:{}\n",
                     data_pdu->originating_entt_.sim_addr_.site_id_.get(),
                     data_pdu->originating_entt_.sim_addr_.app_id_.get(),
                     data_pdu->originating_entt_.entt_id_.get()
                 );
 
-                fmt::print(
+                ss << fmt::format(
                     "  receiving_entt={}:{}:{}\n",
                     data_pdu->receiving_entt_.sim_addr_.site_id_.get(),
                     data_pdu->receiving_entt_.sim_addr_.app_id_.get(),
                     data_pdu->receiving_entt_.entt_id_.get()
                 );
 
-                fmt::print(
+                ss << fmt::format(
                     "  request_id={}, padding={}, num_of_fixed_datum={}, "
                     "num_of_variable_datum={}\n",
                     data_pdu->request_id_.get(),
@@ -437,9 +444,9 @@ namespace {
                 const auto fixed_data = reinterpret_cast<::FixedDatum*>(
                     recv_buffer_.data() + sizeof(::DataPdu)
                 );
-                fmt::print("Fixed data ({})\n", fixed_data_num);
+                ss << fmt::format("Fixed data ({})\n", fixed_data_num);
                 for (size_t i = 0; i < fixed_data_num; ++i) {
-                    fmt::print(
+                    ss << fmt::format(
                         "  id={}, value={}\n",
                         fixed_data[i].datum_id_.get(),
                         fixed_data[i].datum_value_.get()
@@ -447,6 +454,7 @@ namespace {
                 }
             }
 
+            SPDLOG_INFO(ss.str().substr(0, ss.str().size() - 1));
             this->start_recv();
         }
 
@@ -467,7 +475,12 @@ namespace {
 int main() {
     asio::io_context io_context;
     ::UdpServer server(io_context);
-    io_context.run();
+    std::thread th([&] { io_context.run(); });
+
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        SPDLOG_INFO("Running...");
+    }
 
     return 0;
 }
